1f7b40f8946529905cd129f37d333a71f418521e
lisekatimoteus <lisekatimoteus@gmail.com>
2019-09-13 01:24:53 +0700

    Outline Rancangan HTML 1

---
+++ 03/about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>about</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos eligendi harum est facere, suscipit officia culpa ab voluptates libero. Praesentium error modi corrupti nulla! Necessitatibus consequuntur suscipit vero explicabo et?</p>
+</body>
+</html>
